c4e9363075fe2a9d96b0a441f29db7cd744f3622
evelientje90 <evelina.kupryte1990@gmail.com>
2022-01-09 12:03:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>
-  ;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Werather</h1>
+        <footer>
+          This project was coded by Evelina Kupryte and is{" "}
+        <a
+        href="https://github.com/evelientje90/react-app-weather-project"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+      </a>{" "}
+      and{" "}
+      <a
+       href="https://elated-mestorf-fe049f.netlify.app//"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+    hosted on Netlify
+  </a>
+</footer>
+</div>
+    </div>
+  );
 }
