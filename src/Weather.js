2625346b4a84599e6bcc7cde688d084aca2c3b2c
evelientje90 <evelina.kupryte1990@gmail.com>
2022-01-09 20:32:36 +0100

    Project css updated

--- src/Weather.js
+++ src/Weather.js
@@ -2,18 +2,25 @@ import React from  "react";
 import "./Weather.css";
 export default function Weather() {
     return (
-        <div classNme="Weather">
+        <div className="Weather">
             <h1>Amsterdam</h1>
             <ul>
                 <li>Sunday 21:00</li>
                 <li>Sunny</li>
             </ul>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                    alt="Sunny"/>
-                    6°C
-                </div>
+									<div className="float-left">
+                    <img 
+										src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                    alt="Sunny"
+										/>
+									<div className="float-left">
+                    <span className="temperature">6</span>
+                    <span className="unit">°C</span>
+                	</div>
+								</div>
+								</div>
                 <div className="col-6">
                     <ul>
                         <li>Wind: 13km/h</li>
@@ -21,6 +28,24 @@ export default function Weather() {
                     </ul>
                 </div>
             </div>
+            <form>
+                <div className="row">
+                <div className="col-9">
+                    <input 
+                    type="search"
+                    placeholder="Type a city..."
+                    className="form-control"
+                    autoFocus="on"
+                    />
+                </div>
+                <div className="col-3">
+                    <input 
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary w-100" />
+                </div>
+                </div>
+            </form>
         </div>
     )
 }
