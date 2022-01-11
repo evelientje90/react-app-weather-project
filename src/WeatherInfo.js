14d056642d7fcddb0e4981a2c9411e2a7b910677
evelientje90 <evelina.kupryte1990@gmail.com>
2022-01-11 19:48:31 +0100

    Unit conversion added

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,33 +1,37 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 import FormattedDate from "./FormattedDate";
 
-export default function WeatherInfo(props){
+export default function WeatherInfo(props) {
     return (
-        <div className="WeatherInfo">
+      <div className="WeatherInfo">
         <h1>{props.data.city}</h1>
+        <ul>
+          <li>
+            <FormattedDate date={props.data.date} />
+          </li>
+          <li className="text-capitalize">{props.data.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <div className="float-left">
+                <WeatherIcon code={props.data.icon} size={52} />
+              </div>
+  
+              <div className="float-left">
+                <WeatherTemperature celsius={props.data.temperature} />
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
             <ul>
-                <li><FormattedDate date={props.data.date}/></li>
-                <li className="text-capitalize">{props.data.description}</li>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {Math.round(props.data.wind)} km/h</li>
             </ul>
-            <div className="row mt-3">
-                <div className="col-6">
-                    <div className="clearfix">
-                        <div className="float-left">
-                            <WeatherIcon code={props.data.icon} size={52} />
-                        </div>
-					<div className="float-left">
-                        <span className="temperature">{Math.round(props.data.temperature)}</span>
-                        <span className="unit">°C</span>
-					</div>
-				</div>
-                </div>
-            	<div className="col-6">
-                    <ul>
-                        <li>Wind: {Math.round(props.data.wind)}km/h</li>
-                        <li>Humidity: {props.data.humidity}%</li>
-                    </ul>
-				</div>
-                </div>
-			</div>
-       );}
+          </div>
+        </div>
+      </div>
+    );
+  }
