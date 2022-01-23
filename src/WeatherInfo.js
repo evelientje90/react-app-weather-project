ec01472673894b0c4d8484b4c13fc11a43eb6677
evelientje90 <evelina.kupryte1990@gmail.com>
2022-01-23 21:12:54 +0100

    Style updates added

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -8,25 +8,25 @@ export default function WeatherInfo(props) {
     <div className="container">
         <div className="WeatherInfo">
             <div className="row">
-                <div className="col-sm-6 mb-5">
+                <div className="col-sm-4 mb-5">
                     <h1>{props.data.city}</h1>
                 </div>
-                <div className="col-sm-6 mb-5">
+                <div className="col-sm-8 mb-5">
                     <h2><FormattedDate date={props.data.date} /></h2>
                 </div>
             </div>
             <div className="row">
-                <div className="col-sm-6">
+                <div className="col-sm-4">
                     <ul>
                     <li className="text-capitalize">{props.data.description}</li>
                     <li>Humidity: {props.data.humidity}%</li>
                     <li>Wind: {Math.round(props.data.wind)} km/h</li>
                     </ul>
                 </div>
-                <div className="col-sm">
+                <div className="col-sm-4">
                     <WeatherTemperature celsius={props.data.temperature} />
                 </div>
-                <div className="col-sm">
+                <div className="col-sm-4">
                     <WeatherIcon className="" code={props.data.icon} size={52} />
                 </div>
             </div>
